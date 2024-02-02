75d1aadff48af64d01bbb85443c62b1b73559611
Ayhal <ayhalvas2001@gmail.com>
2024-02-02 19:16:01 +0900

    ?

--- config/firebase.js
+++ config/firebase.js
@@ -2,16 +2,25 @@
 import { initializeApp, } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
+// import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: API_KEY,
+//   authDomain: AUTH_DOMAIN,
+//   projectId: PROJECT_ID,
+//   storageBucket: STORAGE_BUCKET,
+//   messagingSenderId: MESSAGING_SENDER_ID,
+//   appId: APP_ID
+// };
+
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: AUTH_DOMAIN,
-  projectId: PROJECT_ID,
-  storageBucket: STORAGE_BUCKET,
-  messagingSenderId: MESSAGING_SENDER_ID,
-  appId: APP_ID
+  apiKey: "AIzaSyD5iNUcWNj7uIw9QfZDsegM9bvN0YoE2n0",
+  authDomain: "diary-db-online.firebaseapp.com",
+  projectId: "diary-db-online",
+  storageBucket: "diary-db-online.appspot.com",
+  messagingSenderId: "130257172686",
+  appId: "1:130257172686:web:e063260497a3e43be7389f"
 };
 
 // Initialize Firebase
